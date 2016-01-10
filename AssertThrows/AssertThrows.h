452d32d2b3e67c98c3ea6223f354ce7ab2e439d1
Witold Skibniewski <witold.skibniewski@tivix.com>
2016-01-10 18:52:20 +0100

    Initial Commit

---
+++ AssertThrows/AssertThrows.h
@@ -0,0 +1,19 @@
+//
+//  AssertThrows.h
+//  AssertThrows
+//
+//  Created by WITOLD SKIBNIEWSKI on 10/01/16.
+//  Copyright © 2016 WITOLD SKIBNIEWSKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssertThrows.
+FOUNDATION_EXPORT double AssertThrowsVersionNumber;
+
+//! Project version string for AssertThrows.
+FOUNDATION_EXPORT const unsigned char AssertThrowsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssertThrows/PublicHeader.h>
+
+
